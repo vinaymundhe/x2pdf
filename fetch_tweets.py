ca026c6a6959541735d5b83dcd30693644c285f3
Vinay Mundhe <vinay.mundhe19@gmail.com>
2025-05-28 13:16:55 +0530

    tweets fetch limit set to 10

--- fetch_tweets.py
+++ fetch_tweets.py
@@ -18,7 +18,7 @@ def get_user_id(username):
     response.raise_for_status()
     return response.json()['data']['id']
 
-def get_tweets(user_id, max_results=100):
+def get_tweets(user_id, max_results=10):
     url = f"https://api.twitter.com/2/users/{user_id}/tweets"
     headers = create_headers()
     params = {
